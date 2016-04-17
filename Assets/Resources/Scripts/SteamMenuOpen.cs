6aa40870be1a159af6e9cf882d3d62ab0a3b4641
Holiday Course <holiday@ad.aie.edu>
2016-04-17 16:57:29 +1000

    Changed UI to include notification when steam menu is open in VR(Experimental untested)

---
+++ Assets/Resources/Scripts/SteamMenuOpen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class SteamMenuOpen : MonoBehaviour {
+    
+    bool systemMenuOpen = false;
+    public GameObject steamOpenIcon;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 1; i < 16; i++)
+        {
+            var device = SteamVR_Controller.Input(i);
+            if(device.GetPressDown(EVRButtonId.k_EButton_System))
+            {
+                //It's now up
+                systemMenuOpen = !systemMenuOpen;
+            }
+        }
+        if(systemMenuOpen == true)
+        {
+            steamOpenIcon.SetActive(true);
+        }
+        else
+        {
+            steamOpenIcon.SetActive(false);
+        }
+
+    }
+}
