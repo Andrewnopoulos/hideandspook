238c3cd633e84bdd4c67a873120de7ffa9e0c607
Tyler Perry <Tyler Perry>
2016-04-30 09:30:25 +0100

    Added toggle spectator mode.

---
+++ Assets/Resources/Scripts/ToggleSpectatorMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleSpectatorMode : MonoBehaviour {
+    public GameObject spectatorModeUI;
+    public GameObject staticBackGround;
+
+    private bool spectatorMode = true;
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.S))
+        {
+            spectatorMode = !spectatorMode;
+        }
+        
+        spectatorModeUI.SetActive(spectatorMode);
+        staticBackGround.SetActive(!spectatorMode);
+    }
+}
